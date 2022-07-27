58b186e67f84cb932e851920918603b4ba0dd0b9
PauloMurakami <ppmura13@gmail.com>
2022-07-27 07:45:11 -0300

    Adicionando cors para desenvolvimento local

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import "reflect-metadata";
 import express from "express";
 import "./db/dataSource"
+import cors from 'cors';
 import { routes } from "./routes";
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:4200'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 app.use(express.json())
+
+app.use(cors(options));
 app.use(routes)
 
 app.listen(3000, () =>{
